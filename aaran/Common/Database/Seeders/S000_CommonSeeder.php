6aa56c3356c7671381ee2ac2b97757d2e6034651
SUNDAR <aaransoftware@gmail.com>
2025-02-06 22:33:14 +0530

    #16 refactor Common migration

---
+++ aaran/Common/Database/Seeders/S000_CommonSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Aaran\Common\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class S000_CommonSeeder extends Seeder
+{
+    public static function run(): void
+    {
+       S101_CitySeeder::run();
+       S102_StateSeeder::run();
+       S103_CountrySeeder::run();
+       S104_PincodeSeeder::run();
+       S105_HsncodeSeeder::run();
+       S106_UnitSeeder::run();
+       S107_CategorySeeder::run();
+       S108_ColourSeeder::run();
+       S109_SizeSeeder::run();
+       S110_DepartmentSeeder::run();
+       S111_TransportSeeder::run();
+       S112_BankSeeder::run();
+       S113_ReceipttypeSeeder::run();
+       S114_DespatcheSeeder::run();
+       S115_GstPercentSeeder::run();
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,51 +2,15 @@
 
 namespace Database\Seeders;
 
-use Aaran\Common\Database\Seeders\S101_CitySeeder;
-use Aaran\Common\Database\Seeders\S102_StateSeeder;
-use Aaran\Common\Database\Seeders\S103_CountrySeeder;
-use Aaran\Common\Database\Seeders\S104_PincodeSeeder;
-use Aaran\Common\Database\Seeders\S105_HsncodeSeeder;
-use Aaran\Common\Database\Seeders\S106_UnitSeeder;
-use Aaran\Common\Database\Seeders\S107_CategorySeeder;
-use Aaran\Common\Database\Seeders\S108_ColourSeeder;
-use Aaran\Common\Database\Seeders\S109_SizeSeeder;
-use Aaran\Common\Database\Seeders\S110_DepartmentSeeder;
-use Aaran\Common\Database\Seeders\S111_TransportSeeder;
-use Aaran\Common\Database\Seeders\S112_BankSeeder;
-use Aaran\Common\Database\Seeders\S113_ReceipttypeSeeder;
-use Aaran\Common\Database\Seeders\S114_DespatcheSeeder;
-use Aaran\Core\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Aaran\Common\Database\Seeders\S000_CommonSeeder;
+use Aaran\Core\Database\Seeders\S00_CoreSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-//        // User::factory(10)->create();
-//
-//        S101_CitySeeder::run();
-//        S102_StateSeeder::run();
-//        S103_CountrySeeder::run();
-//        S104_PincodeSeeder::run();
-//        S105_HsncodeSeeder::run();
-//        S106_UnitSeeder::run();
-//        S107_CategorySeeder::run();
-//        S108_ColourSeeder::run();
-//        S109_SizeSeeder::run();
-//        S110_DepartmentSeeder::run();
-//        S111_TransportSeeder::run();
-//        S112_BankSeeder::run();
-//        S113_ReceipttypeSeeder::run();
-//        S114_DespatcheSeeder::run();
-//
-//        User::factory()->create([
-//            'name' => 'Test User',
-//            'email' => 'test@example.com',
-//        ]);
+        S00_CoreSeeder::run();
+        S000_CommonSeeder::run();
     }
 }
