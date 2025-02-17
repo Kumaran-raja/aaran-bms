4c560cc048bef5841b316bc068460268ba08e012
SUNDAR <aaransoftware@gmail.com>
2025-02-17 09:08:02 +0530

    #42 added larecipe for documentation

--- aaran/Docs/routes.php
+++ aaran/Docs/routes.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 //Master
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 
-    Route::get('/docs', Aaran\Web\Livewire\Dashboard\Index::class)->name('docs');
+//    Route::get('/docs', Aaran\Web\Livewire\Dashboard\Index::class)->name('docs');
 
 });
